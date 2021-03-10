fcd34d89b7d206cbd275d4d80ab601c5f0b0cf11
Suraj Wate <suraj.wate@gmail.com>
2021-03-10 09:40:27 +0530

    adding template

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+@app.route('/food')
+def food():
+    return render_template('add_food.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
