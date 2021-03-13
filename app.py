f6a969a7a9a39dd36c42d7d212f214305025aad7
Suraj Wate <suraj.wate@gmail.com>
2021-03-13 10:37:48 +0530

    display all foods in the database

--- app.py
+++ app.py
@@ -3,31 +3,39 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 def connect_db():
     sql = sqlite3.connect('D:\\Suraj\\Coding\\python\\databases\\food_log.db')
     sql.row_factory = sqlite3.Row
     return sql
+
 
 def get_db():
     if not hasattr(g, 'sqlite3_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+
 @app.route('/')
 def index():
     return render_template('home.html')
+
 
 @app.route('/view')
 def view():
     return render_template('day.html')
 
-@app.route('/food', methods=[ 'POST', 'GET'])
+
+@app.route('/food', methods=['POST', 'GET'])
 def food():
+    db = get_db()
+
     if request.method == 'POST':
         name = request.form['food-name']
         protein = int(request.form['protein'])
@@ -35,11 +43,16 @@ def food():
         fat = int(request.form['fat'])
         calories = protein*4 + carbohydrates*4 + fat*9
 
-        db = get_db()
-        db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?,?,?,?,?)', \
-            [name, protein, carbohydrates, fat, calories])
+        db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?,?,?,?,?)',
+                   [name, protein, carbohydrates, fat, calories])
         db.commit()
-    return render_template('add_food.html')
+
+    cur = db.execute(
+        'select name, protein, carbohydrates, fat, calories from food')
+    results = cur.fetchall()
+
+    return render_template('add_food.html', results=results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
