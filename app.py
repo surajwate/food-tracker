969799ba6d824201ffed292bc4294041624ca1dc
Suraj Wate <suraj.wate@gmail.com>
2021-03-14 17:29:54 +0530

    adding food to database (in food_date table)

--- app.py
+++ app.py
@@ -31,14 +31,16 @@ def index():
 
         dt = datetime.strptime(date, '%Y-%m-%d')
         database_date = datetime.strftime(dt, '%Y%m%d')
-        db.execute('insert into log_date (entry_date) values (?)', [database_date])
+        db.execute('insert into log_date (entry_date) values (?)',
+                   [database_date])
         db.commit()
-    
-    cur = db.execute('select entry_date from log_date order by entry_date desc')
+
+    cur = db.execute(
+        'select entry_date from log_date order by entry_date desc')
     results = cur.fetchall()
 
     pretty_results = []
-    
+
     for i in results:
         single_date = {}
         d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
@@ -47,22 +49,26 @@ def index():
     return render_template('home.html', results=pretty_results)
 
 
-@app.route('/view/<date>', methods=['GET', 'POST']) #date is from database, format eg: 20180728
+# date is from database, format eg: 20180728
+@app.route('/view/<date>', methods=['GET', 'POST'])
 def view(date):
-    if request.method == 'POST':
-        return "<h1>The food item added is #{}".format(request.form['food-select'])
-        
     db = get_db()
-    cur = db.execute('select entry_date from log_date where entry_date = ?', [date])
-    result = cur.fetchone()
+    cur = db.execute(
+        'select id, entry_date from log_date where entry_date = ?', [date])
+    date_result = cur.fetchone()
 
-    d = datetime.strptime(str(result['entry_date']), '%Y%m%d')
+    if request.method == 'POST':
+        db.execute('insert into food_date (food_id, log_date_id) values (?, ?)', [
+                   request.form['food-select'], date_result['id']])
+        db.commit()
+
+    d = datetime.strptime(str(date_result['entry_date']), '%Y%m%d')
     pretty_date = datetime.strftime(d, '%B %d, %Y')
 
     food_cur = db.execute('select id, name from food')
     food_results = food_cur.fetchall()
 
-    return render_template('day.html',date=pretty_date, food=food_results)
+    return render_template('day.html', date=pretty_date, food=food_results)
 
 
 @app.route('/food', methods=['POST', 'GET'])
